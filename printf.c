8de289649968f8b92c5a8fa8260f8440a9b946e7
Sami <samiztsouhailsami@gmail.com>
2023-07-22 21:09:49 +0100

    damn

--- printf.c
+++ printf.c
@@ -1,12 +1,37 @@
 #include "main.h"
 /**
+ * get_specifier - gets the specifier
+ * @format: list of argument types passed to the function
+ * @input: list of arguments passed to the function
+ * @i: pointer to the index of format
+ * @len: pointer to the number of characters printed
+ * Return: void
+ */
+int get_specifier(char c, va_list input)
+{
+	int len = 0;
+
+	if (c == 'c')
+		len += _putchar(va_arg(input, int));
+	else if (c == 's')
+		len += _puts(va_arg(input, char *));
+	else if (c == '%')
+		len += _putchar('%');
+	else
+	{
+		len += _putchar('%');
+		len += _putchar(c);
+	}
+	return (len);
+}
+/**
  * _printf - prints anything
  * @format: list of argument types passed to the function
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, len = 0;
+	int i = 0 , len = 0;
 	va_list input;
 
 
@@ -22,18 +47,7 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == '%')
-			{
-				len += _putchar('%'); /* if format is %% print % */
-			}
-			else if (format[i] == 'c')
-			{
-				len += _putchar(va_arg(input, int)); /* if format is %c print char */
-			}
-			else if (format[i] == 's')
-			{
-				len += _puts(va_arg(input, char *)); /* if format is %s print string */
-			}
+			get_specifier(format[i], input);
 		}
 		else
 		{
