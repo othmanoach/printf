1e55c8016be844a490fccd9689dc8718fdfd708f
othmanoach <othmanoach@gmail.com>
2023-07-22 15:40:10 -0700

    function printf

--- printf.c
+++ printf.c
@@ -25,6 +25,8 @@ int helper_func(char specifier, va_list input)
 	return (len);
 }
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - prints anything
  * @format: list of argument types passed to the function
@@ -58,4 +60,16 @@ int _printf(const char *format, ...)
 	}
 	va_end(input);
 	return (len);
+}
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
