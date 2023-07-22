c9d85d3463095d5c43729be09fa079d66ab7d816
Sami <samiztsouhailsami@gmail.com>
2023-07-22 19:46:19 +0100

    test

--- main.c
+++ main.c
@@ -6,60 +6,6 @@ int main()
 {
 	int len_1 = 0, len_2 = 0;
 
-	/* ===========> %c <=========== */
-	printf("----------------------\n");
-	printf("----> CHAR CASES <----\n");
-	printf("----------------------\n");
-	len_1 = printf("Expected output:    %c\n", 53);
-	len_2 = _printf("Current output:     %c\n", 53);
-	printf("Expected length:    [%d]\n", len_1);
-	printf("Current length:     [%d]\n", len_2);
-	len_1 = printf("Expected output:    %cc\n", 'a');
-	len_2 = _printf("Current output:     %cc\n", 'a');
-	printf("Expected length:    [%d]\n", len_1);
-	printf("Current length:     [%d]\n", len_2);
-	len_1 = printf("Expected output:    %cAAA\n", 'a');
-	len_2 = _printf("Current output:     %cAAA\n", 'a');
-	printf("Expected length:    [%d]\n", len_1);
-	printf("Current length:     [%d]\n", len_2);
-	len_1 = printf("Expected output:    %yd\n");
-	len_2 = _printf("Current output:     %yd\n");
-	printf("Expected length:    [%d]\n", len_1);
-	printf("Current length:     [%d]\n", len_2);
-	len_1 = printf("Expected output:    %c\n", '\0');
-	len_2 = _printf("Current output:     %c\n", '\0');
-	printf("Expected length:    [%d]\n", len_1);
-	printf("Current length:     [%d]\n", len_2);
-	len_1 = printf("Expected output:    %%%c\n", 'y');
-	len_2 = _printf("Current output:     %%%c\n", 'y');
-	printf("Expected length:    [%d]\n", len_1);
-	printf("Current length:     [%d]\n", len_2);
-
-	/* ===========> %s <=========== */
-	printf("----------------------\n");
-	printf("---> STRING CASES <---\n");
-	printf("----------------------\n");
-	len_1 = printf("Expected output:    %s\n", "Morocco");
-	len_2 = _printf("Current output:     %s\n", "Morocco");
-	printf("Expected length:    [%d]\n", len_1);
-	printf("Current length:     [%d]\n", len_2);
-	len_1 = printf("Expected output:    %s$\n", "");
-	len_2 = _printf("Current output:     %s$\n", "");
-	printf("Expected length:    [%d]\n", len_1);
-	printf("Current length:     [%d]\n", len_2);
-	len_1 = printf("Expected output:    %s$\n", NULL);
-	len_2 = _printf("Current output:     %s$\n", NULL);
-	printf("Expected length:    [%d]\n", len_1);
-	printf("Current length:     [%d]\n", len_2);
-	len_1 = printf("Expected output:    %s\n", "hello, world");
-	len_2 = _printf("Current output:     %s\n", "hello, world");
-	printf("Expected length:    [%d]\n", len_1);
-	printf("Current length:     [%d]\n", len_2);
-	len_1 = printf("Expected output:    %sForLife\n", "Morocco");
-	len_2 = _printf("Current output:     %sForLife\n", "Morocco");
-	printf("Expected length:    [%d]\n", len_1);
-	printf("Current length:     [%d]\n", len_2);
-
 	/* ===========> % <=========== */
 	printf("----------------------\n");
 	printf("-> PERCENTAGE CASES <-\n");
