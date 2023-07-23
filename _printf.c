f6a9c1067b589b5d084a0694ba8ca4266dd62a93
othmanoach <othmanoach@gmail.com>
2023-07-22 17:20:31 -0700

    printf function

---
+++ _printf.c
@@ -0,0 +1,73 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+
+/**
+ * helper_func - helper function for _printf
+ * @specifier: format specifier
+ * @input: input list
+ * Return: number of characters printed
+ */
+int helper_func(char specifier, va_list input)
+{
+	int len = 0;
+
+	if (specifier == '%')
+	{
+		len += _putchar('%'); /* if format is %% print % */
+	}
+	else if (specifier == 'c')
+	{
+		len += _putchar(va_arg(input, int)); /* if format is %c print char */
+	}
+	else if (specifier == 's')
+	{
+		len += _puts(va_arg(input, char *)); /* if format is %s print string */
+	}
+	else if (specifier == 'd' || specifier == 'i')
+	{
+		len += print_int(va_arg(input, int)); /* if format is %d or %i print int */
+	}
+	else
+	{
+		len += _putchar('%'); /* print the '%' character itself */
+		len += _putchar(specifier); /* print the unrecognized format specifier */
+	}
+
+	return len;
+}
+
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, len = 0;
+	va_list input;
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1); /* if format is null or if format is % return -1 */
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1); /* if format is % return -1 */
+
+	va_start(input, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			len += helper_func(format[i], input);
+		}
+		else
+		{
+			len += _putchar(format[i]); /* if format is not % print char */
+		}
+		i++;
+	}
+
+	va_end(input);
+	return (len);
+}
