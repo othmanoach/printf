059311b8537d5e12800b061c311238c3741f58a0
othmanoach <othmanoach@gmail.com>
2023-07-25 05:59:44 -0700

    add space + -

--- functions2.c
+++ functions2.c
@@ -89,55 +89,55 @@ int print_octal(unsigned int n)
 }
 /**
  * print_plus_int - Helper function to print integer with '+' flag
- * @num: Integer to print
+ * @n: Integer to print
  * Return: Number of characters printed
  */
-int print_plus_int(int num)
+int print_plus_int(int n)
 {
     int len = 0;
 
-    if (num >= 0)
+    if (n >= 0)
         len += _putchar('+');
-    len += print_int(num);
+    len += print_int(n);
     return (len);
 }
 /**
  * print_space_int - Helper function to print integer with space flag
- * @num: Integer to print
+ * @n: Integer to print
  * Return: Number of characters printed
  */
-int print_space_int(int num)
+int print_space_int(int n)
 {
     int len = 0;
 
-    if (num >= 0)
+    if (n >= 0)
         len += _putchar(' ');
-    len += print_int(num);
+    len += print_int(n);
     return (len);
 }
 /**
  * print_plus_unsigned_int - Helper function to print unsigned integer with '+' flag
- * @num: Unsigned integer to print
+ * @n: Unsigned integer to print
  * Return: Number of characters printed
  */
-int print_plus_unsigned_int(unsigned int num)
+int print_plus_unsigned_int(unsigned int n)
 {
     int len = 0;
 
     len += _putchar('+');
-    len += print_unsigned_int(num);
+    len += print_unsigned_int(n);
     return (len);
 }
 /**
  * print_space_unsigned_int - Helper function to print unsigned integer with space flag
- * @num: Unsigned integer to print
+ * @n: Unsigned integer to print
  * Return: Number of characters printed
  */
-int print_space_unsigned_int(unsigned int num)
+int print_space_unsigned_int(unsigned int n)
 {
     int len = 0;
 
     len += _putchar(' ');
-    len += print_unsigned_int(num);
+    len += print_unsigned_int(n);
     return (len);
 }
