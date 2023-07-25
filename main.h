fbeedca1b796ffebf9b41c757937948be0c44ebd
othmanoach <othmanoach@gmail.com>
2023-07-25 05:31:16 -0700

    handle + space -

--- main.h
+++ main.h
@@ -21,6 +21,12 @@ int print_hex_l(unsigned long int n);
 int print_mem_address(void *addr);
 int print_rev(char *s);
 int rot13(char *s);
+int print_plus_int(int num);
+int print_space_int(int num);
+int print_plus_unsigned_int(unsigned int num);
+int print_space_unsigned_int(unsigned int num);
+int print_hash_octal(unsigned int num);
+int print_hash_hex(unsigned int num, int uppercase);
 
 
 #endif
