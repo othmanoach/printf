208ac65aa9a754f7edee62897007836b37c8f053
othmanoach <othmanoach@gmail.com>
2023-07-25 07:33:34 -0700

    fixing the octal / hex / space / +

--- printf.c
+++ printf.c
@@ -3,15 +3,15 @@
 #include <stddef.h>
 
 /**
- * helper_func - Helper function for _printf
- * @specifier: Format specifier
- * @input: Input list
- * @flag_plus: Flag for the '+' character
- * @flag_space: Flag for the ' ' character
- *
- * Return: Number of characters printed
+ * helper_func - helper function for _printf
+ * @specifier: format specifier
+ * @input: input list
+ * @flag_plus: flag for '+'
+ * @flag_space: flag for ' '
+ * @flag_hash: flag for '#'
+ * Return: number of characters printed
  */
-int helper_func(char specifier, va_list input, int flag_plus, int flag_space)
+int helper_func(char specifier, va_list input, int flag_plus, int flag_space, int flag_hash)
 {
     int len = 0;
 
@@ -32,13 +32,36 @@ int helper_func(char specifier, va_list input, int flag_plus, int flag_space)
             len += print_int(num);
     }
     else if (specifier == 'u')
-        len += print_unsigned_int(va_arg(input, unsigned int));
+    {
+        unsigned int unum = va_arg(input, unsigned int);
+        if (flag_plus)
+            len += print_plus_unsigned_int(unum);
+        else if (flag_space)
+            len += print_space_unsigned_int(unum);
+        else
+            len += print_unsigned_int(unum);
+    }
     else if (specifier == 'o')
-        len += print_octal(va_arg(input, unsigned int));
+    {
+        unsigned int unum = va_arg(input, unsigned int);
+        if (flag_hash)
+            len += _putchar('0');
+        len += print_octal(unum);
+    }
     else if (specifier == 'x')
-        len += print_hex(va_arg(input, unsigned int));
+    {
+        unsigned int unum = va_arg(input, unsigned int);
+        if (flag_hash)
+            len += _puts("0x");
+        len += print_hex(unum);
+    }
     else if (specifier == 'X')
-        len += print_HEX(va_arg(input, unsigned int));
+    {
+        unsigned int unum = va_arg(input, unsigned int);
+        if (flag_hash)
+            len += _puts("0X");
+        len += print_HEX(unum);
+    }
     else if (specifier == 'p')
         len += print_mem_address(va_arg(input, void *));
     else if (specifier == 'b')
@@ -51,29 +74,27 @@ int helper_func(char specifier, va_list input, int flag_plus, int flag_space)
         len += print_rev(va_arg(input, char *));
     else
     {
-        len += _putchar('%'); /* Print the '%' character itself */
-        len += _putchar(specifier); /* Print the unrecognized format specifier */
+        len += _putchar('%'); /* print the '%' character itself */
+        len += _putchar(specifier); /* print the unrecognized format specifier */
     }
 
     return (len);
 }
 
 /**
- * _printf - Prints anything with custom format specifiers
- * @format: List of argument types passed to the function
- * Return: Number of characters printed
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
     int i = 0, len = 0;
     va_list input;
-    int flag_plus = 0;
-    int flag_space = 0;
 
     if (!format || (format[0] == '%' && !format[1]))
-        return (-1);
+        return (-1); /* if format is null or if format is % return -1 */
     if (format[0] == '%' && format[1] == ' ' && !format[2])
-        return (-1);
+        return (-1); /* if format is % return -1 */
 
     va_start(input, format);
 
@@ -81,24 +102,26 @@ int _printf(const char *format, ...)
     {
         if (format[i] == '%')
         {
+            int flag_plus = 0, flag_space = 0, flag_hash = 0;
             i++;
 
-            flag_plus = 0;
-            flag_space = 0;
-            while (format[i] == '+' || format[i] == ' ')
+            while (format[i] == '+' || format[i] == ' ' || format[i] == '#')
             {
                 if (format[i] == '+')
                     flag_plus = 1;
                 else if (format[i] == ' ')
                     flag_space = 1;
+                else if (format[i] == '#')
+                    flag_hash = 1;
+
                 i++;
             }
 
-            len += helper_func(format[i], input, flag_plus, flag_space);
+            len += helper_func(format[i], input, flag_plus, flag_space, flag_hash);
         }
         else
         {
-            len += _putchar(format[i]);
+            len += _putchar(format[i]); /* if format is not % print char */
         }
         i++;
     }
